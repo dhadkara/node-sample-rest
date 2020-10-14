d568eb13891d5d61d8cbb9aa7d1b32495e3d4a13
Dhadkar <dhadkar@3c22fb0827aa.ant.amazon.com>
2020-10-13 21:08:16 -0500

    fixing eslinting

--- src/handlers/get-by-id.js
+++ src/handlers/get-by-id.js
@@ -5,6 +5,7 @@ const tableName = process.env.SAMPLE_TABLE;
 
 // Create a DocumentClient that represents the query to add an item
 const dynamodb = require('aws-sdk/clients/dynamodb');
+
 const docClient = new dynamodb.DocumentClient();
 
 // eslint-disable-next-line valid-jsdoc
@@ -12,32 +13,32 @@ const docClient = new dynamodb.DocumentClient();
  * A simple example includes a HTTP get method to get one item by id from a DynamoDB table.
  */
 exports.getByIdHandler = async (event) => {
-    if (event.httpMethod !== 'GET') {
-        throw new Error(`getMethod only accept GET method, you tried: ${event.httpMethod}`);
-    }
-    // All log statements are written to CloudWatch
-    // eslint-disable-next-line no-console
-    console.info('received:', event);
+  if (event.httpMethod !== 'GET') {
+    throw new Error(`getMethod only accept GET method, you tried: ${event.httpMethod}`);
+  }
+  // All log statements are written to CloudWatch
+  // eslint-disable-next-line no-console
+  console.info('received:', event);
 
-    // Get id from pathParameters from APIGateway because of `/{id}` at template.yml
-    const id = event.pathParameters.id;
+  // Get id from pathParameters from APIGateway because of `/{id}` at template.yml
+  const { id } = event.pathParameters;
 
-    // Get the item from the table
-    // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#get-property
-    var params = {
-        TableName: tableName,
-        Key: {id: id}
-    };
-    const data = await docClient.get(params).promise();
-    const item = data.Item;
+  // Get the item from the table
+  // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#get-property
+  const params = {
+    TableName: tableName,
+    Key: { id },
+  };
+  const data = await docClient.get(params).promise();
+  const item = data.Item;
 
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify(item)
-    };
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(item),
+  };
 
-    // All log statements are written to CloudWatch
-    // eslint-disable-next-line no-console
-    console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`);
-    return response;
+  // All log statements are written to CloudWatch
+  // eslint-disable-next-line no-console
+  console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`);
+  return response;
 };
